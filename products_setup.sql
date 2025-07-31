2275918273aed86415cb0810f9d9e36e5b1c56f9
atuly31 <atuly3051@gmail.com>
2025-07-31 12:42:47 +0530

    Added Foreign key

--- products_setup.sql
+++ products_setup.sql
@@ -22,3 +22,27 @@ IGNORE 1 ROWS
 (id, cost, category, name, brand, retail_price, department, sku, distribution_center_id);
 
 select * from products;
+create Table departments (
+                             id INT auto_increment primary key,
+                             name varchar (100)  Unique not null
+);
+
+
+insert Into departments (name)
+select distinct department from products;
+
+
+Alter table products add column department_id INT;
+
+SET SQL_SAFE_UPDATES = 0;
+
+update products p
+    join departments d on p.department = d.name
+    Set p.department_id = d.id;
+
+Alter Table products drop  column department;
+
+Alter Table products
+    Add constraint fk_department
+        foreign key (department_id)
+            references departments(id)
